2cbb79caf7385866fd3c3d8dfe6a7510a9244aff
lzl <zhenlong.li@xiangguang.com>
2018-01-25 16:39:50 +0800

    修改正式机接口地址

--- src/api/baseConfig.js
+++ src/api/baseConfig.js
@@ -1,9 +1,11 @@
 /**
  * Created by lzl on 17/6/5.
+ * 部署去掉切换用户和接口地址
  */
 const baseConfig = {
-  baseURL: 'http://10.5.1.253:10022'// 10066
-  // baseURL: 'http://dingtalk.gmkholdings.com'
+  // baseURL: 'http://10.5.1.253:10022'// 10066
+  baseURL: 'https://dingtalk.gmkholdings.com'
+  // baseURL: 'https://dingtalk.gmkholdings.com:8080'
 }
 
 module.exports = baseConfig;
